2d37761ecaea8a88037d2221286dcbee9a33a0ee
Adri <adrifarnal@gmail.com>
2026-02-24 14:07:14 +0100

    Remove import

--- app/src/main/java/com/trackbool/bookreader/ui/BookListScreen.kt
+++ app/src/main/java/com/trackbool/bookreader/ui/BookListScreen.kt
@@ -1,6 +1,5 @@
 package com.trackbool.bookreader.ui
 
-import android.content.Context
 import android.net.Uri
 import android.provider.OpenableColumns
 import androidx.activity.compose.rememberLauncherForActivityResult
