1b5668e8ae46e3cd4d842f06ca811d7169d06e88
Adri <adrifarnal@gmail.com>
2026-03-03 12:18:31 +0100

    Use coil cache

--- app/src/main/java/com/trackbool/bookreader/di/CoilModule.kt
+++ app/src/main/java/com/trackbool/bookreader/di/CoilModule.kt
@@ -2,6 +2,8 @@ package com.trackbool.bookreader.di
 
 import android.content.Context
 import coil.ImageLoader
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
 import com.trackbool.bookreader.ui.EpubImageFetcher
 import dagger.Module
 import dagger.Provides
@@ -20,8 +22,20 @@ object CoilModule {
         return ImageLoader.Builder(context)
             .components {
                 add(EpubImageFetcher.Factory())
-                // add(PdfImageFetcher.Factory())
             }
+            .memoryCache {
+                MemoryCache.Builder(context)
+                    .maxSizePercent(0.25)
+                    .build()
+            }
+            .diskCache {
+                DiskCache.Builder()
+                    .directory(context.cacheDir.resolve("image_cache"))
+                    .maxSizePercent(0.10)
+                    .build()
+            }
+            .respectCacheHeaders(false)
+            .crossfade(true)
             .build()
     }
 }
