5d41202390c3d14c942292887219a0382d6571f6
Adri <adrifarnal@gmail.com>
2026-03-04 11:23:35 +0100

    Remove unused classes

--- app/src/main/java/com/trackbool/bookreader/BookReaderApplication.kt
+++ app/src/main/java/com/trackbool/bookreader/BookReaderApplication.kt
@@ -1,15 +1,7 @@
 package com.trackbool.bookreader
 
 import android.app.Application
-import coil.ImageLoader
-import coil.ImageLoaderFactory
 import dagger.hilt.android.HiltAndroidApp
-import jakarta.inject.Inject
 
 @HiltAndroidApp
-class BookReaderApplication : Application(), ImageLoaderFactory {
-    @Inject
-    lateinit var imageLoader: ImageLoader
-
-    override fun newImageLoader() = imageLoader
-}
+class BookReaderApplication : Application()
