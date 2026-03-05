4482fe1eb4d6ec1c4d67dd19941ef8639a2d1152
Adri <adrifarnal@gmail.com>
2026-03-05 10:54:29 +0100

    Add option for webview debugging

--- app/src/main/java/com/trackbool/bookreader/BookReaderApplication.kt
+++ app/src/main/java/com/trackbool/bookreader/BookReaderApplication.kt
@@ -1,7 +1,16 @@
 package com.trackbool.bookreader
 
 import android.app.Application
+import android.webkit.WebView
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class BookReaderApplication : Application()
+class BookReaderApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            WebView.setWebContentsDebuggingEnabled(true)
+        }
+    }
+}
